7936bc918d77befbf7b214891181baeeccde9639
sooto selim <adewaleselim6@gmail.com>
2023-02-22 13:29:21 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var btn = document.getElementById('btn');
+var togg=document.getElementById('toggle')
+function toggleBtn(){
+    btn.classList.toggle("active")
+    togg.classList.toggle("active")
+}
